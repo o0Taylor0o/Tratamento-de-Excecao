0b50db10fe892a968adda96b2beb21e0517f0a28
o0Taylor0o <62898130+o0Taylor0o@users.noreply.github.com>
2020-03-31 17:25:25 -0300

    Update RoboInteligente.java

--- 1° Trabalho (Java Exception)/src/RoboInteligente.java
+++ 1° Trabalho (Java Exception)/src/RoboInteligente.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-	public class RoboInteligente extends Robo{
+public class RoboInteligente extends Robo{
+	Random r = new Random();
+	List <Integer> r2 = new ArrayList <Integer>();
+		
 		public RoboInteligente(String cor, int eixoX, int eixoY) {
 			super(cor, eixoX, eixoY);
 		}
@@ -9,18 +15,29 @@
 				this.eixoX += 1;
 				System.out.println();
 			}else if(movimento == 2) {
-				if(eixoY < 0) {
-					throw new MovimentoInvalidoException(eixoX, eixoY);
-				}
+					this.eixoY -= 1;
+					if(eixoY < 0) {
+						r2.add(1);
+						r2.add(3);
+						movimento = r2.get(new Random().nextInt(r2.size()));
+						eixoY = 0;
+							throw new MovimentoInvalidoException(eixoX, eixoY);
+					}
+					System.out.println();
 			}else if(movimento == 3) {
 					this.eixoY += 1;
 					System.out.println();
 			}else if(movimento == 4) {
-				if(eixoX < 0) {
-					throw new MovimentoInvalidoException(eixoX, eixoY);
+					this.eixoX -= 1;
+					if(eixoX < 0) {
+						r2.add(1);
+						r2.add(3);
+						movimento = r2.get(new Random().nextInt(r2.size()));
+						eixoX = 0;
+							throw new MovimentoInvalidoException(eixoX, eixoY);
 				}
 					}
-			}
+			}	
 		
 		public void matrizRobo() {
 			m = new char[5][5];
@@ -59,4 +76,5 @@
 		public void posicaoInteligente() {
 			System.out.println("Robo inteligente de cor " + cor + " esta na posicao " + eixoX + " no eixo X e na posicao " + eixoY + " no eixo Y.");
 		}
-	}
+		
+}
